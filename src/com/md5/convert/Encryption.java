e4ba6e4ea6d7bab9055e7b9e98cd0d50284c6a93
zsl <zsl@DESKTOP-9218VR0>
2017-03-18 17:35:25 +0800

    加注释

---
+++ src/com/md5/convert/Encryption.java
@@ -0,0 +1,34 @@
+package com.md5.convert;
+
+import java.util.Scanner;
+
+import com.nkl.common.util.Md5;
+/*
+ * 用户请自行加入jar包
+ */
+public class Encryption {
+	
+	public static void main(String[] args) {
+		String password = null;
+		String md5 = null;
+		String confirm = null;
+		Scanner scanner= new Scanner(System.in);
+		
+		System.out.println("将要进行数据加密吗（y/n?）：");
+		confirm = scanner.next();	
+		
+		while(confirm == "Y"||confirm == "y")
+		{
+			System.out.println("请输入要加密的数据：");
+			password = scanner.next();
+			
+			md5 = Md5.makeMd5(password);
+			System.out.println("密文是："+md5);
+			
+			System.out.println("将要进行数据加密吗（y/n?）：");
+			confirm = scanner.next();	
+		}
+		
+		System.out.println("谢谢使用！");
+	}
+}
